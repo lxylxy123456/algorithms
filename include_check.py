7da310207eba53671d61c83caf961340963249a4
李宵逸 <13691419520@163.com>
2018-07-14 13:51:15 +0800

    include_check.py

---
+++ include_check.py
@@ -0,0 +1,28 @@
+import os, re
+
+for i in os.listdir() :
+	if not i.endswith('.cpp') :
+		continue
+	a = open(i).read()
+	try :
+		for j in re.findall('#include <(\w+)>', a) :
+			if j == 'cassert' :
+				assert 'assert(' in a
+			elif j == 'algorithm' :
+				assert 'std::max_element' in a
+			elif j == 'cmath' :
+				assert 'pow' in a
+			elif j == 'iomanip' :
+				assert 'std::left' in a
+			elif j == 'exception' :
+				assert 'std::invalid_argument' in a
+			elif j == 'map' :
+				assert 'std::map' in a or 'std::pair' in a
+			elif j in ['list', 'set'] :
+				assert 'std::' + j in a
+			else :
+				assert(False)
+	except AssertionError :
+		print('in:', i)
+		print('include:', j)
+
