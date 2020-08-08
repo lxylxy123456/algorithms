a2a57b2e1bcfeb1717e0023d96325fda3b239f3a
李宵逸 <13691419520@163.com>
2020-08-08 08:56:36 -0700

    Add CountingSortTest.cpp

---
+++ src/CountingSortTest.cpp
@@ -0,0 +1,57 @@
+//
+//  algorithm - some algorithms in "Introduction to Algorithms", third edition
+//  Copyright (C) 2020  lxylxy123456
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "CountingSort.hpp"
+#include "utils.hpp"
+
+#include <cassert>
+
+using namespace algorithms;
+
+#include "InsertSort.hpp"
+
+template <typename T>
+bool is_increasing_order(T p, T r) {
+	if (p == r)
+		return true;
+	for (T i = p, j = p + 1; j < r; j++)
+		if (*i > *j)
+			return false;
+	return true;
+}
+
+int test(int n, int k) {
+	std::vector<int> a;
+	random_integers(a, 0, k - 1, n);
+	std::vector<int> b(a), c(n, 0);
+	InsertSort(a);
+	output_integers(a);
+	CountingSort(b, c, k, Self<int>);
+	output_integers(c);
+	assert(a == c);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	std::vector<int> ns{0, 1, 2, 5, 8, 19, 52, 100};
+	std::vector<int> ks{1, 2, 5, 8, 19, 52, 100};
+	for (std::vector<int>::iterator n = ns.begin(); n < ns.end(); n++)
+		for (std::vector<int>::iterator k = ks.begin(); k < ks.end(); k++)
+			test(*n, *k);
+	return 0;
+}
