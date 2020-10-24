8fc7244f01257206fe10bc7032a9228d8454559b
李宵逸 <13691419520@163.com>
2020-10-24 00:00:30 -0700

    Add AllPairsShortestPathsTest.cpp

--- src/AllPairsShortestPathsTest.cpp
+++ src/AllPairsShortestPathsTest.cpp
@@ -16,10 +16,10 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-#include "FloydWarshall.hpp"
+#include "AllPairsShortestPaths.hpp"
 #include "utils.hpp"
 
-#include "BellmanFord.hpp"
+#include "FloydWarshall.hpp"
 #include <cassert>
 
 using namespace algorithms;
@@ -33,39 +33,11 @@ int test(size_t v, size_t e) {
 	G.graphviz();
 	Matrix<Weight<int>> W(v, v);
 	G.to_matrix(W);
-	auto ans = FloydWarshall(W);
-	Matrix<Weight<int>> D = ans.first;
-	Matrix<T_ptr<size_t>> PI = ans.second;
-	GraphAdjList<size_t> GG(dir);
-	umap<Edge<size_t>, int, EdgeHash<size_t>> w;
-	for (size_t i = 0; i < v; i++) {
-		GG.add_vertex(i);
-		for (size_t j = 0; j < v; j++)
-			if (i != j && !G.get_edge(i, j).inf) {
-				GG.add_edge(i, j);
-				w[Edge<size_t>(i, j, dir)] = G.get_edge(i, j).val;
-			}
-	}
-	for (size_t i = 0; i < v; i++) {
-		umap<size_t, RelaxInfo<size_t, int>> ans_bf;
-		assert(BellmanFord(GG, w, i, ans_bf));
-		for (size_t j = 0; j < v; j++)
-			if (i != j) {
-				std::vector<size_t> ans;
-				PrintAllPairsShortestPath(PI, i, j, ans);
-				if (ans.size()) {
-					assert(!ans_bf[j].d.inf && D[i][j].val == ans_bf[j].d.val);
-					int d = 0;
-					for (std::vector<size_t>::iterator k = ans.begin();
-						k + 1 < ans.end(); k++) {
-						assert(!G.get_edge(*k, *(k + 1)).inf);
-						d += G.get_edge(*k, *(k + 1)).val;
-					}
-					assert(d == D[i][j].val);
-				} else
-					assert(ans_bf[j].d.inf);
-			}
-	}
+	Matrix<Weight<int>> ans1 = SlowAllPairsShortestPaths(W);
+	Matrix<Weight<int>> ans2 = FasterAllPairsShortestPaths(W);
+	Matrix<Weight<int>> ans_fw = FloydWarshall(W).first;
+	assert(ans1 == ans2);
+	assert(ans1 == ans_fw);
 	return 0;
 }
 
