7adac2bba0dee6c6f7ac0d11d811c1ba7e3e677e
李宵逸 <13691419520@163.com>
2020-10-24 00:08:02 -0700

    Add TransitiveClosureTest

---
+++ src/TransitiveClosureTest.cpp
@@ -0,0 +1,52 @@
+//
+//  algorithm - some algorithms in "Introduction to Algorithms", third edition
+//  Copyright (C) 2020  lxylxy123456
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "TransitiveClosure.hpp"
+#include "utils.hpp"
+
+#include "FloydWarshall.hpp"
+#include <cassert>
+
+using namespace algorithms;
+
+int test(size_t v, size_t e) {
+	const bool dir = 1;
+	const int weight_lower = 0;
+	const int weight_upper = e;
+	WeightedAdjMatrix<size_t, int> G(dir);
+	G.random_graph(v, e, weight_lower, weight_upper);
+	G.graphviz();
+	Matrix<bool> ans = TransitiveClosure(G);
+	// FloydWarshall
+	Matrix<Weight<int>> W(v, v);
+	G.to_matrix(W);
+	Matrix<Weight<int>> ans_fw = FloydWarshall(W).first;
+	for (size_t i = 0; i < v; i++)
+		for (size_t j = 0; j < v; j++)
+			assert((ans[i][j] == 1) == (ans_fw[i][j].inf == 0));
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	std::vector<size_t> m = {5, 10, 23, 49, 100};
+	for (std::vector<size_t>::iterator v = m.begin(); v < m.end(); v++)
+		for (std::vector<size_t>::iterator e = m.begin(); e < m.end(); e++)
+			for (int n = 0; n < 5; n++)
+				test(*v, *e);
+	return 0;
+}
