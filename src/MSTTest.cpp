38a8507155112c3c1511c2bc3a5f32f3a077a446
李宵逸 <13691419520@163.com>
2020-10-23 22:45:59 -0700

    Add MSTTest.cpp

---
+++ src/MSTTest.cpp
@@ -0,0 +1,91 @@
+//
+//  algorithm - some algorithms in "Introduction to Algorithms", third edition
+//  Copyright (C) 2020  lxylxy123456
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "MST.hpp"
+#include "utils.hpp"
+
+#include "DisjointSet.hpp"
+#include <cassert>
+
+using namespace algorithms;
+
+int test(size_t v, size_t e) {
+	const bool dir = 0;
+	GraphAdjList<size_t> G(dir);
+	random_graph(G, v, e);
+	umap<Edge<size_t>, size_t, EdgeHash<size_t>> w;
+	random_weight(G, w, (size_t) 0, e - 1);
+	uset<Edge<size_t>, EdgeHash<size_t>> A;
+	MSTKruskal(G, w, A);
+	umap<size_t, MSTPrimInfo<size_t, size_t>> ans;
+	MSTPrim(G, w, *G.V.begin(), ans);
+	DisjointSetForest<size_t> F_kruskal, F_prim, F_all;
+	size_t span_kruskal = 0, span_prim = 0;
+	graphviz(G);
+	for (auto i = G.V.begin(); i != G.V.end(); i++) {
+		F_kruskal.MakeSet(*i);
+		F_prim.MakeSet(*i);
+		F_all.MakeSet(*i);
+	}
+	for (auto i = G.all_edges(); !i.end(); i++) {
+		bool ans_kruskal = (A.find(*i) != A.end());
+		bool ans_prim = ((!ans[i.s()].pi_nil && ans[i.s()].pi == i.d()) ||
+						(!ans[i.d()].pi_nil && ans[i.d()].pi == i.s()));
+		if (ans_kruskal) {
+			span_kruskal += w[*i];
+			assert(F_kruskal.FindSet(i.s()) != F_kruskal.FindSet(i.d()));
+			F_kruskal.Union(i.s(), i.d());
+		}
+		if (ans_prim) {
+			span_prim += w[*i];
+			assert(F_prim.FindSet(i.s()) != F_prim.FindSet(i.d()));
+			F_prim.Union(i.s(), i.d());
+		}
+		F_all.Union(i.s(), i.d());
+	}
+	assert(span_kruskal == span_prim);
+	for (auto i = G.V.begin(); i != G.V.end(); i++) {
+		for (auto j = G.V.begin(); j != G.V.end(); j++) {
+			assert((F_all.FindSet(*i) == F_all.FindSet(*j)) ==
+					(F_kruskal.FindSet(*i) == F_kruskal.FindSet(*j)));
+			assert((F_all.FindSet(*i) == F_all.FindSet(*j)) ==
+					(F_prim.FindSet(*i) == F_prim.FindSet(*j)));
+		}
+	}
+	auto f1 = [](size_t v) { return false; };
+	auto f2 = [w, ans, A](Edge<size_t> e) mutable {
+		std::cout << " [label=\"" << w[e] << "\"";
+		if ((!ans[e.s].pi_nil && ans[e.s].pi == e.d) ||
+			(!ans[e.d].pi_nil && ans[e.d].pi == e.s))
+			std::cout << " style=bold";
+		if (A.find(e) != A.end())
+			std::cout << " color=red";
+		std::cout << "]";
+	};
+	graphviz(G, f1, f2);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	std::vector<size_t> m = {5, 10, 23, 49, 100};
+	for (std::vector<size_t>::iterator v = m.begin(); v < m.end(); v++)
+		for (std::vector<size_t>::iterator e = m.begin(); e < m.end(); e++)
+			for (int n = 0; n < 5; n++)
+				test(*v, *e);
+	return 0;
+}
