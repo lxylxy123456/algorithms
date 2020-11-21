e04616dfbcd440e752cc251d31e6dad1824dbe94
李宵逸 <13691419520@163.com>
2020-11-21 00:59:51 -0800

    Add SimplexTest.cpp

--- src/SimplexTest.cpp
+++ src/SimplexTest.cpp
@@ -1,6 +1,6 @@
 //
 //  algorithm - some algorithms in "Introduction to Algorithms", third edition
-//  Copyright (C) 2018  lxylxy123456
+//  Copyright (C) 2020  lxylxy123456
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as
@@ -17,15 +17,24 @@
 //
 
 #include "Simplex.hpp"
-
-#include <cassert>
 #include "utils.hpp"
+
 #include "LUPSolve.hpp"
+#include <cassert>
 
 using namespace algorithms;
 
+bool almost_equal(double lhs, double rhs) {
+	return fabs(lhs - rhs) < 1e-7;
+}
+
 template <typename T>
-void main_T(const size_t n, const size_t m) {
+bool almost_equal(Fraction<T> lhs, Fraction<T> rhs) {
+	return lhs == rhs;
+}
+
+template <typename T>
+int test(size_t m, size_t n) {
 	std::vector<int> int_a, int_b, int_c;
 	random_integers(int_a, -m, m, m * n);
 	random_integers(int_b, -m, m, m);
@@ -51,19 +60,50 @@ void main_T(const size_t n, const size_t m) {
 	for (size_t i = 1; i <= n; i++)
 		std::cout << c[i] << " ";
 	std::cout << std::endl;
-	vectst x = Simplex(A, b, c);
-	for (size_t i = 1; i <= n; i++)
-		std::cout << x[i] << " ";
+	try {
+		vectst x = Simplex(A, b, c);
+		for (size_t i = 1; i <= n; i++)
+			std::cout << x[i] << " ";
+		std::cout << std::endl;
+		for (size_t i = 1; i <= m; i++) {
+			T s(0);
+			for (size_t j = 1; j <= n; j++)
+				s += A[n + i][j] * x[j];
+			assert(s - b[n + i] < T(1) / T(100000));
+		}
+	} catch (std::invalid_argument& e) {
+		std::cout << e.what() << std::endl;
+		assert(std::string(e.what()) == "infeasible" ||
+				std::string(e.what()) == "unbounded");
+	}
 	std::cout << std::endl;
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
-	const size_t type = get_argv(argc, argv, 1, 0);
-	const size_t m = get_argv(argc, argv, 2, 4);
-	const size_t n = get_argv(argc, argv, 3, 2);
-	if (!type)
-		main_T<double>(n, m);
-	else
-		main_T<Fraction<int>>(n, m);
+	std::cout << random_seed_get() << std::endl;
+	std::vector<size_t> ns = {2, 5, 8, 19, 39, /*52, 73, 100*/};
+	for (std::vector<size_t>::iterator m = ns.begin(); m < ns.end(); m++)
+		for (std::vector<size_t>::iterator n = ns.begin(); n < ns.end(); n++)
+			if (*m > *n)
+				test<double>(*m, *n);
+	test<Fraction<long long int>>(3, 2);
+	test<Fraction<long long int>>(4, 2);
+	test<Fraction<long long int>>(4, 3);
+	/*
+	TODO: sometimes Assertion `l != NIL' fails
+	e.g. 
+		4 -7 7 -5 -8 
+		-6 -8 -7 2 0 
+		-8 4 4 -6 8 
+		-4 -5 -7 7 8 
+		4 -5 7 -3 -7 
+		-6 -7 7 -1 -8 
+		7 2 1 -3 6 
+		-5 3 4 -6 4 
+
+		-4 1 1 -8 1 0 3 4 
+		1 6 -3 4 5 
+	*/
 	return 0;
 }
