54b3fd6bd69cd1564076b64d2a8176d7ca8567c4
李宵逸 <13691419520@163.com>
2020-12-20 14:17:14 -0800

    Remove redundant random_seed_get()

--- src/BTreeTest.cpp
+++ src/BTreeTest.cpp
@@ -95,7 +95,6 @@ int test(size_t n, size_t k) {
 
 int main(int argc, char *argv[]) {
 	parse_args(argc, argv);
-	std::cout << random_seed_get() << std::endl;
 	std::vector<int> ns = {1, 4, 16, 19, 20, 25, 100};
 	std::vector<int> ks = {3, 4, 5, 6, 7, 8};
 	for (std::vector<int>::iterator n = ns.begin(); n < ns.end(); n++)
--- src/SimplexTest.cpp
+++ src/SimplexTest.cpp
@@ -89,7 +89,6 @@ int test(size_t m, size_t n, T epsilon) {
 
 int main(int argc, char *argv[]) {
 	parse_args(argc, argv);
-	std::cout << random_seed_get() << std::endl;
 	std::vector<size_t> ns = {2, 5, 8, 19, 39, 52, 73};
 	for (std::vector<size_t>::iterator m = ns.begin(); m < ns.end(); m++)
 		for (std::vector<size_t>::iterator n = ns.begin(); n < ns.end(); n++)
