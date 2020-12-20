332f03bf3058f112bf70bd12bea10577fa0df838
李宵逸 <13691419520@163.com>
2020-12-19 21:43:33 -0800

    Show random seed

--- src/FordFulkersonTest.cpp
+++ src/FordFulkersonTest.cpp
@@ -66,6 +66,7 @@ int test(size_t v, size_t e) {
 }
 
 int main(int argc, char *argv[]) {
+	std::cerr << random_seed_get() << std::endl;
 	std::vector<size_t> m = {5, 10, 23, 49, 100};
 	for (std::vector<size_t>::iterator v = m.begin(); v < m.end(); v++)
 		for (std::vector<size_t>::iterator e = m.begin(); e < m.end(); e++)
