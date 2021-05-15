620f7e8a9a681cf8e031bda9e1b6d3308d623e8e
李宵逸 <13691419520@163.com>
2021-05-15 16:24:39 -0700

    debuged

--- include/FordFulkerson.hpp
+++ include/FordFulkerson.hpp
@@ -54,7 +54,7 @@ void FordFulkerson(GT& G, umap_WT& c, T s, T t, umap_WT& f) {
 		}
 		umap<T, BFSInfo<T>> BFS_ans;
 		BFS(Gf, s, BFS_ans);
-		std::vector<int> p;
+		std::vector<T> p;
 		PrintPath(s, t, BFS_ans, p);
 		if (!p.size())
 			break;
